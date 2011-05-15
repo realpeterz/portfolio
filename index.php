1d2a8bac1a7f3d7eb3f53a024245eb15a779338d
Peter Zhang <realpeterz@hotmail.com>
2011-05-14 22:30:01 -0400

    Just a starting point

---
+++ index.php
@@ -0,0 +1,310 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+
+  <!-- 1140px Grid styles for IE -->
+  <!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
+  <!-- The 1140px Grid - http://cssgrid.net/ -->
+  <link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />	
+  <!-- CSS: implied media="all" -->
+  <link rel="stylesheet" href="css/style.css?v=2">
+
+  <!-- Uncomment if you are specifically targeting less enabled mobile browsers
+  <link rel="stylesheet" media="handheld" href="css/handheld.css?v=2">  -->
+
+  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+
+  <script src="js/libs/modernizr-1.7.min.js"></script>
+
+	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
+	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+
+</head>
+<body>
+
+<header class="container">
+	<div class="row">
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="onecol last">
+			<p>One</p>
+		</div>
+	</div>
+</header>
+
+<section class="container">
+	<div class="row">
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="twocol last">
+			<p>Two columns</p>
+		</div>
+	</div>
+</section>
+
+<div class="container">
+	<div class="row">
+		<div class="threecol">
+			<p>Three columns</p>
+		</div>
+		<div class="threecol">
+			<p>Three columns</p>
+		</div>
+		<div class="threecol">
+			<p>Three columns</p>
+		</div>
+		<div class="threecol last">
+			<p>Three columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="fourcol">
+			<p>Four columns</p>
+		</div>
+		<div class="fourcol">
+			<p>Four columns</p>
+		</div>
+		<div class="fourcol last">
+			<p>Four columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="onecol">
+			<p>One</p>
+		</div>
+		<div class="elevencol last">
+			<p>Eleven columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="twocol">
+			<p>Two columns</p>
+		</div>
+		<div class="tencol last">
+			<p>Ten columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="threecol">
+			<p>Three columns</p>
+		</div>
+		<div class="ninecol last">
+			<p>Nine columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="fourcol">
+			<p>Four columns</p>
+		</div>
+		<div class="eightcol last">
+			<p>Eight columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="fivecol">
+			<p>Five columns</p>
+		</div>
+		<div class="sevencol last">
+			<p>Seven columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="sixcol">
+			<p>Six columns</p>
+		</div>
+		<div class="sixcol last">
+			<p>Six columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="sevencol">
+			<p>Seven columns</p>
+		</div>
+		<div class="fivecol last">
+			<p>Five columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="eightcol">
+			<p>Eight columns</p>
+		</div>
+		<div class="fourcol last">
+			<p>Four columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="ninecol">
+			<p>Nine columns</p>
+		</div>
+		<div class="threecol last">
+			<p>Three columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="tencol">
+			<p>Ten columns</p>
+		</div>
+		<div class="twocol last">
+			<p>Two columns</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="elevencol">
+			<p>Eleven columns</p>
+		</div>
+		<div class="onecol last">
+			<p>One</p>
+		</div>
+	</div>
+</div>
+
+<footer class="container">
+	<div class="row">
+		<div class="threecol">
+			<p>Three columns</p>
+		</div>
+		<div class="sixcol">
+			<p>Six columns</p>
+		</div>
+		<div class="threecol last">
+			<p>Three columns</p>
+		</div>
+	</div>
+</footer>
+
+
+</body>
+
+
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+  <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
+
+
+  <!-- scripts concatenated and minified via ant build script-->
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+  <!-- end scripts-->
+
+
+  <!--[if lt IE 7 ]>
+    <script src="js/libs/dd_belatedpng.js"></script>
+    <script>DD_belatedPNG.fix("img, .png_bg"); // Fix any <img> or .png_bg bg-images. Also, please read goo.gl/mZiyb </script>
+  <![endif]-->
+
+
+  <!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
+  <script>
+    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s)}(document,"script"));
+  </script>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# My porfolio under construction
+## Tentative Features
+- Fluid layout down to mobile with 1140 grid
+-	Cross-browser compatability with HTML5 Boilerplate
+- Single page with animated scrolling
+- Grid view for portfolio (defaulty and only for mobile)
+- Horizontal accordion for portfolio (default for desktop) 
+- Contextual pagination with sausage.js
+- Email form
+## Visuals
+- Ribbon on top for name
+- Texture background with glow in each section
+- Social icons: twitter, delicious, linkedin 
