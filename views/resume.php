bb0dee6762e6296da218eb5e6d881cab120aa2fd
Peter Zhang <realpeterz@hotmail.com>
2011-06-24 03:38:22 -0500

    resume HTML done

--- views/resume.php
+++ views/resume.php
@@ -1,4 +1,125 @@
     <section class="anchor row" id="resume">
         <div class="sprite-ribbon"><h2>Resume</h2></div>
+        <div class="row">
+        
+            <div id="work" class="sixcol">
+                <h2>Work Experience</h2>
+                <ul class="resume-section">
+                    <li class="timeline"><span class="timeline-head">Rebel Idealist LLC.</span><span class="time">12/2010-04/2011</span>
+                        <ul class="detail-list">
+                            <li class="detail-item"><span class="detail-title">Position:</span></li>
+                            <li class="detail-item"><span class="detail-title">Responsibilities:</span>
+                                
+                            </li>
+                            <li class="detail-item"><span class="detail-title">Projects:</span>
+                                <ul class="detail-sublist">
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                </ul>
+                            </li> 
+                        </ul>
+                    
+                    </li>
+                    <li class="timeline"><span class="timeline-head">OpenText Corporation</span><span class="time">10/2010-12/2010</span>
+                        <ul class="detail-list">
+                            <li class="detail-item"><span class="detail-title">Position:</span></li>
+                            <li class="detail-item"><span class="detail-title">Responsibilities:</span>
+                                
+                            </li>
+                            <li class="detail-item"><span class="detail-title">Projects:</span>
+                                <ul class="detail-sublist">
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                </ul>
+                            </li> 
+                        </ul>
+                        
+                    </li>
+                    
+                    <li class="timeline"><span class="timeline-head">Shipley and Associates Inc.</span><span class="time">06/2010-08/2010</span>
+                        <ul class="detail-list">
+                            <li class="detail-item"><span class="detail-title">Position:</span></li>
+                            <li class="detail-item"><span class="detail-title">Responsibilities:</span>
+                                
+                            </li>
+                            <li class="detail-item"><span class="detail-title">Projects:</span>
+                                <ul class="detail-sublist">
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                </ul>
+                            </li> 
+                        </ul>
+                    </li>
+                    
+                    <li class="timeline"><span class="timeline-head">Beijing Municipal Transportation Administration Bureau</span><span class="time">06/2004-06/2008</span>
+                        <ul class="detail-list">
+                            <li class="detail-item"><span class="detail-title">Position:</span></li>
+                            <li class="detail-item"><span class="detail-title">Responsibilities:</span>
+                                
+                            </li>
+                            <li class="detail-item"><span class="detail-title">Projects:</span>
+                                <ul class="detail-sublist">
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                    <li class="detail-subitem"></li>
+                                </ul>
+                            </li> 
+                        </ul>
+                    </li>
+                </ul>
+            </div> <!-- end of #work -->
+        
+        
+        
+        
+            <div class="sixcol last">
+                <div id="education">
+                    <h2 class="headline">Education</h2>
+                    <ul class="resume-section">
+                        <li class="timeline"><span class="timeline-head">University of Texas at Austin</span><span class="time">01/2009 - 05/2011</span>                       
+                              <ul class="detail-list">
+                                   <li class="detail-item"><span class="detail-title">Department:</span><span class="detail-content">School of Information</span></li>
+                                   <li class="detail-item"><span class="detail-title">Major:</span><span class="detail-content">Information Architecture</span></li>
+                                   <li class="detail-item"> <span class="detail-title">Degree:</span><span class="detail-content">Master of Science in Information Studies</span></li>
+                               </ul>                           
+                        </li>
+                        
+                        
+                        <li class="timeline"><span class="timeline-head">Beijing Information Science and Technology University</span><span class="time">09/2000 - 06/2004</span>                 
+                                <ul class="detail-list">
+                                    <li class="detail-item"><span class="detail-title">Department:</span><span class="detail-content">Technology & Business Management</span></li>
+                                    <li class="detail-item"> <span class="detail-title">Major:</span><span class="detail-content">Management Information Systems</span></li>
+                                    <li class="detail-item"><span class="detail-title">Degree:</span><span class="detail-content">Bachelor of Science in MIS</span></li>
+                                </ul>
+                        </li>
+                     </ul>
+                </div>
+                
+                <div id="skills">
+                    <h2>Skills & Tools</h2>
+                    <ul class="detail-list">
+                        <li class="detail-item">Front-end: HTML(5), CSS(3), Sass, LESS, JavaScript, XSL</li>
+                        <li class="detail-item">Back-end: Object-Oriented PHP, Ruby</li>
+                        <li class="detail-item">Database: MySQL, SQLite, CouchDB</li>
+                        <li class="detail-item">Frameworks & libraries: Codeigniter, Ruby on Rails(learning), jQuery, Backbone.js, 960 Grid System, Blueprint(CSS), Compass(Sass)</li>
+                        <li class="detail-item">Version Control: Git, Subversion </li>
+                        <li class="detail-item">Wireframing: Balsamiq, OmniGraffle, Visio, Google Drawings, Pen&paper</li>
+                        <li class="detail-item">Visuals: Photoshop, GIMP</li>
+                        <li class="detail-item">Usability: Camstudio, WebEx</li>
+                        <li class="detail-item">Operating Systems: Linux, Mac OS X, Windows</li>
+                    </ul>
+                </div>
+                
+            </div> <!-- end of sixcol last -->
+            
+            
+        </div> <!-- end of row -->
     </section>
 </div> <!-- end of #content -->
